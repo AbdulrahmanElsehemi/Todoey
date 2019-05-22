648a4d3f980cea879c6677266c6a48f7d6e0e8f0
abdulrahman elsehemy <a.elsehemy10@gmail.com>
2019-05-23 00:23:50 +0200

    MVC Design pattern

---
+++ Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Abdelrahman on 5/22/19.
+//  Copyright © 2019 Abdelrahman. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
